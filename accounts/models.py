032167cf8d9a1e14565080f17460c79d9b956f81
jungyeojin <yeojin9905@naver.com>
2023-11-05 13:16:47 +0900

    feat: admin 기능

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser, Group # AbstractUser와 Group을 상속받아서 사용자 모델을 커스텀
+from django.contrib.auth.models import AbstractUser, BaseUserManager # AbstractUser와 Group을 상속받아서 사용자 모델을 커스텀
+
+class UserManager(BaseUserManager): #superuser 생성을 위한 클래스
+    use_in_migrations = True
+
+    def _create_user(self, userId, password, **extra_fields):
+        if not userId:
+            raise ValueError('The given userId must be set')
+        user = self.model(userId=userId, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, userId, password=None, **extra_fields):
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(userId, password, **extra_fields)
+
+    def create_superuser(self, userId, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+        return self._create_user(userId, password, **extra_fields)
 
 class User(AbstractUser):
     username = None
@@ -10,4 +34,5 @@ class User(AbstractUser):
     contact = models.CharField(max_length=20)
     email = models.EmailField(max_length=50)
     isForwarder = models.BooleanField(default=False)
+    objects = UserManager()
     USERNAME_FIELD = 'userId' # 로그인 시 사용할 필드를 지정
